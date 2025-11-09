470c8431f15bf129e28b4520239cc96b98acbd13
schy-dev <shashanks9644@gmail.com>
2025-11-09 20:18:32 +0530

    Update next.config.ts

--- front-end/next-frontend/next.config.ts
+++ front-end/next-frontend/next.config.ts
@@ -2,13 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // ✅ Allow builds to complete even if there are type errors
     ignoreBuildErrors: true,
   },
   eslint: {
-    // (Optional) Skip ESLint during build for faster CI/CD
     ignoreDuringBuilds: true,
   },
+  output: "standalone",
 };
 
 export default nextConfig;
