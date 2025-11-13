de42f5aeab69b75fd59b5dee6affa594ced892dd
schy-dev <shashanks9644@gmail.com>
2025-11-13 23:24:51 +0530

    fix

--- lib/axiosClient.ts
+++ lib/axiosClient.ts
@@ -6,7 +6,7 @@ const API_BASE = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:4000';
 console.log(process.env.NEXT_PUBLIC_API_URL, "API_BASE");
 export const axiosClient = axios.create({
   baseURL: API_BASE,
-  withCredentials: false,
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
